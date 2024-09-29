4bcf4d41e2d58179642fe0ae49b36a3d0c7f93de
Sam Smith <jarno.hantula@hotmail.com>
2024-09-29 20:15:12 +0300

    tehtävä 9

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ExpenseItem from './components/Expenses/ExpenseItem';
+
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 
 function App() {
@@ -25,30 +26,14 @@ function App() {
     date: new Date ("2022-05-12"),
    },
   ];
+  
   return (
     <div>
-      <ExpenseItem
-      title={expenses[0].title}
-      amount={expenses[0].amount}
-      date={expenses[0].date}
-      />
-      <ExpenseItem
-      title={expenses[1].title}
-      amount={expenses[1].amount}
-      date={expenses[1].date}
-      />
-      <ExpenseItem
-      title={expenses[2].title}
-      amount={expenses[2].amount}
-      date={expenses[2].date}
-      />
-      <ExpenseItem
-      title={expenses[3].title}
-      amount={expenses[3].amount}
-      date={expenses[3].date}
-      />
-      <p>Jarno Hantula / VAMK / BIT</p>
-    </div>
+      <NewExpense />
+      <Expenses items={expenses}/>
+      <p>Jarno Hantula / Vamk/ BIT</p>
+      </div>
+      
   );
 }
 
