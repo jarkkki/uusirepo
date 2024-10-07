be603886468ee19e86e436a37c0e461ec96cab0f
Jarno Hantula <jarno.hantula@hotmail.com>
2024-10-07 14:13:05 +0300

    tehtävä 17

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
 
@@ -17,19 +17,6 @@ const Expenses = (props) => {
     const filteredExpenses = props.items.filter(expense => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
-
-    let expensesContent = <p>No expenses found.</p>;
-        
-    if (filteredExpenses.length > 0) {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-            />
-        ));
-    }
         
         return (
         <Card className="expenses">
@@ -37,7 +24,7 @@ const Expenses = (props) => {
             selected={filteredYear} 
             onChangeFilter={filterChangeHandler}
             />
-        {expensesContent}    
+            <ExpensesList items={filteredExpenses} />    
         </Card>
     );
 }
